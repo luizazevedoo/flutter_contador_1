34b2bd30aa2a8327b211dd28db404d17595ca021
luizazevedoo <mluiza.a.azevedo@gmail.com>
2022-09-16 16:13:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(title: "Contador de pessoas", home: Home())); //Material App
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  int _pessoa = 0;
+  String _mensagem = "Bem-vindo, pode entrar!";
+  void _changePeople(int delta) {
+    setState(() {
+      _pessoa += delta;
+      if (_pessoa >= 20) {
+        _mensagem = "Está lotado, desculpe, não pode entrar.";
+        _pessoa = 20;
+      } else if (_pessoa < 0) {
+        _mensagem = "Bem-vindo, pode entrar!";
+        _pessoa = 0;
+      } else {
+        _mensagem = "Bem-vindo, pode entrar!";
+      }
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "imagens/fundo.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_pessoa",
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ), //text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    child: const Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    child: const Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _mensagem,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
+            ) //text
+          ], //widget
+        ) //Column
+      ],
+    );
+  }
+}
